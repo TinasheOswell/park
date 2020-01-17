da4d55bec0399e7cdf7183c2cce1978d2ab3c660
TinasheOswell <osiemusariri@gmail.com>
2020-01-17 11:09:36 -0700

    new park file

---
+++ park.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Map:
+    #has tiles 
+    #you is Ishara
+    #me is me
+    def openwindow:
+        pass you
+    def drawtile:
+        #draw the grid
+        #initial all black squares
+        pass me
+    def makevalid:
+        #makes a tile valid for car motion etc
+        pass you
+    
+    def makeparkingspots:
+        #uses (x,y) coordinates
+        #makes certain tiles for parking
+        pass me
+    
+    def freepark:
+        #randomly picks a spot from the parking spaces and makes it the goal
+        pass you
+    
+    def make path:
+        #makes tiles valid for driving
+        pass me
+    
+    
+
+
+# In[ ]:
+
+
+class Tile:
+    #attributes :
+        #color, parking or not path or not you
+        # draws itself me
+        #location and size you
+        
+
+
+# In[ ]:
+
+
+
+

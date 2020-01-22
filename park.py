61b2f210a3a1aa1542dfe15ae55042125c787861
Titaniayuki <57152556+Titaniayuki@users.noreply.github.com>
2020-01-22 11:17:28 -0700

    open window

--- park.py
+++ park.py
@@ -3,35 +3,55 @@
 
 # In[ ]:
 
+import pygame
+def main():
+    worldmap = Map()
+    worldmap.openwindow()
 
 class Map:
+    Window = None
     #has tiles 
     #you is Ishara
     #me is me
-    def openwindow:
-        # this is what i changed
-        pass you
-    def drawtile:
+    def openwindow(self):
+        #opens the window
+        self.Window = pygame.display.set_mode((1000,600))
+        pygame.display.set_caption("Parking")
+
+        #keeps it open until the x is clicked
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+
+    def drawtile(self):
         #draw the grid
         #initial all black squares
-        pass me
-    def makevalid:
+        pass #me
+    def makevalid(self, valid_tile_list):
+        ## how should this work?
+            ##add tiles to a valid tile list OR
+            ##change the tile attribute valid to true??
         # and here
         #makes a tile valid for car motion etc
-        pass you
+        pass #you
     
-    def makeparkingspots:
+    def makeparkingspots(self):
         #uses (x,y) coordinates
         #makes certain tiles for parking
-        pass me
+        pass #me
     
-    def freepark:
+    def freepark(self, parking_center):
+        #make calculation depending on how wide and high the tiles are to
+            ##determine how many tiles to the left and to the right
         #randomly picks a spot from the parking spaces and makes it the goal
-        pass you
+        pass #you
     
-    def make path:
+    def make_path(self):
         #makes tiles valid for driving
-        pass me
+        pass #me
     
     
 
@@ -40,6 +60,7 @@ class Map:
 
 
 class Tile:
+    pass
     #attributes :
         #color, parking or not path or not you
         # draws itself me
@@ -47,8 +68,7 @@ class Tile:
         
 
 
-# In[ ]:
-
+main()
 
 
 

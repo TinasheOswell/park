a4edfbc399fe2702388140e89de03f3c231dde24
TinasheOswell <osiemusariri@gmail.com>
2020-01-24 00:17:49 -0700

    implemented the draw tile method in the Tile class along with other necessary methods and attributes for both map and tile classes

--- park.py
+++ park.py
@@ -13,23 +13,49 @@ class Map:
     #has tiles 
     #you is Ishara
     #me is me
+    
+    def __init__(self):
+        self.map = []
+        self.makeTiles()
+    
     def openwindow(self):
         #opens the window
-        self.Window = pygame.display.set_mode((800,1500))
+        self.Window = pygame.display.set_mode((1500,800))
+        Tile.set_window(self.Window)
         pygame.display.set_caption("Parking")
 
+        self.drawtile() #draws the initial tile display need to move
+        
         #keeps it open until the x is clicked
         running = True
         while running:
+            pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
 
+    def maketile(self, location):
+        tile = Tile(False, False, location)
+        return tile
+    
+    def makeTiles(self):
+        for row_i in range(0, 65):
+            col = []
+            for col_i in range(0, 53):
+                tile = self.maketile([(row_i * 23), (col_i*15)])
+                col.append(tile)
+            self.map.append(col)
+
     def drawtile(self):
         #draw the grid
         #initial all black squares
-        pass #me
+        count = 0
+        for row in self.map:
+            for tile in row:
+                count = count + 1
+                tile.draw(count)
+        #me
 
     def makevalidroad(self, valid_tile_list):
         ## how should this work?
@@ -62,16 +88,25 @@ class Map:
 
 
 class Tile:
+    
+    window = None
+    back_col = pygame.Color(46, 80, 143)
+    boarder_width = 1
+    
+    @classmethod
+    def set_window(cls, window):
+        cls.window = window
 
     def __init__(self, valid_road, valid_park, location):
         self.valid_road = valid_road
         self.valid_park = valid_park
         self.location = location
-
+        self.rectangle = pygame.Rect(location[0], location[1], 23,15)
+        
     def draw(self):
         #draw itself
         #me
-        pass
+        pygame.draw.rect(Tile.window, Tile.back_col, self.rectangle, Tile.boarder_width)
 
 
     #attributes :
